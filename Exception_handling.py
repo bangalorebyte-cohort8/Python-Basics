a2a1eacb7f88273d0e707b3292f8de25ff638cc0
Uday Keith <udaykeith@gmail.com>
2018-02-16 15:38:59 +0530

    Try and Except

---
+++ Exception_handling.py
@@ -0,0 +1,11 @@
+# Exception Handling 
+
+import math
+anumber = int(input("Please enter an integer "))
+
+try:
+       print(math.sqrt(anumber))
+except:
+       print("Bad Value for square root")
+       print("Using absolute value instead")  
+       print(math.sqrt(abs(anumber)))
